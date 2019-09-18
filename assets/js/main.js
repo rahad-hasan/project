f6dc350a5ed14c8a030077d33401b82c89055997
rahad-hasan <55480376+rahad-hasan@users.noreply.github.com>
2019-09-18 14:04:44 +0600

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+
+$('.my-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+	autoplay:true,
+	autoplayTimer:2000,
+	autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+$('.testimonial-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+	autoplay:true,
+	autoplayTimer:3000,
+	autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:1
+        }
+    }
+})
